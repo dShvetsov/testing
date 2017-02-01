f70efdef4264efbcaaee7a5639ba85730e0e6c45
Denis Shvetsov <dshvetsov@arccn.ru>
2017-02-01 19:45:59 +0300

    snooped file

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.insert(0, '/home/dshvetsov/mininet')
+
 
 from mininet.topo import Topo
 from mininet.net import Mininet
-
+from mininet.util import dumpNodeConnections
+from mininet.node import RemoteController
 
 class Node :
     def __init__(self):
@@ -36,6 +40,7 @@ class BinTreeTopo(Topo):
         else:
             self.switchName += 1
             sw = self.addSwitch("s%s" % self.switchName)
+            self.sws.append(sw)
             left_subtree = self.build_tree(node.left)
             right_subtree = self.build_tree(node.right)
             self.addLink(sw, left_subtree)
@@ -43,10 +48,26 @@ class BinTreeTopo(Topo):
             return sw
 
     def __init__(self, n=2):
+        self.sws = []
         self.switchName = 0
         self.hostName= 0
         Topo.__init__( self )
         root = createTopo( n )
         self.build_tree( root )
 
-topos = {'mytopo' : ( lambda n: BinTreeTopo(n) ) }
+def simpleTest():
+    "Simple test"
+    topo = BinTreeTopo(n = 4)
+    net = Mininet(topo, controller=lambda name: RemoteController(name, ip='127.0.0.1', port=6653))
+    net.start()
+    switches = [net.get(i) for i in topo.sws]
+    for j in switches :
+        filename = "snoop" + str( j.dpid ) + '.dmp'
+        j.dpctl('snoop', '2>' + filename + '&')
+    dumpNodeConnections(net.hosts)
+    net.pingAll()
+    net.stop()
+
+if __name__ == '__main__':
+    simpleTest()
+#topos = {'mytopo' : ( lambda n: BinTreeTopo(n) ) }
